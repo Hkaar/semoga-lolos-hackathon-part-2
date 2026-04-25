b351546040ae1de48b86e77530232a930faf0870
dnljnt <designcodeninja@gmail.com>
2026-04-25 23:13:33 +0700

    feat: add Navbar component to Home page and remove it from RootLayout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/hero";
 import Problem from "@/components/problem";
+import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+    <Navbar />
       <Hero />
       <Problem />
     </>
